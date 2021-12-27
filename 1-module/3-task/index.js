4377a685906c5682f8a97fc13252d2f3e59646ba
Vlad <polzovatelu2000@gmail.com>
2021-12-27 15:05:20 +0300

    toUpperCase

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,9 @@
 function ucFirst(str) {
-  // ваш код...
+  if(str.length<=1){
+    return str.toUpperCase()
+  }
+  else{
+    let n = str.slice(0,1).toUpperCase()
+    return str = n+str.slice(1)
+  }
 }
