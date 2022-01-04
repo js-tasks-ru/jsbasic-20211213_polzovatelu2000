efdb9245397a7bc00b0979210223d216a015ec96
Vlad <polzovatelu2000@gmail.com>
2022-01-04 16:58:07 +0300

    Function sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,14 @@
 function sumSalary(salaries) {
-  // ваш код...
+  sum = 0
+  for(let i in salaries){
+    if(typeof salaries[i] == "number"){
+      if(String(salaries[i])==="NaN"||String(salaries[i])==="Infinity"||String(salaries[i])==="-Infinity"){
+        delete salaries[i]
+      }
+      else{
+        sum+=salaries[i]
+      }
+    }
+  }
+  return salaries = sum
 }
