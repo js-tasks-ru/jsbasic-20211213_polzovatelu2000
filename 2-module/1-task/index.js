7e46fb50e9268e4be2f2f71181357c3044227c80
Vlad <polzovatelu2000@gmail.com>
2022-01-04 17:01:06 +0300

    Fix name

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,14 +1,14 @@
 function sumSalary(salaries) {
-  sum = 0
+  let s = 0
   for(let i in salaries){
     if(typeof salaries[i] == "number"){
       if(String(salaries[i])==="NaN"||String(salaries[i])==="Infinity"||String(salaries[i])==="-Infinity"){
         delete salaries[i]
       }
       else{
-        sum+=salaries[i]
+        s+=salaries[i]
       }
     }
   }
-  return salaries = sum
+  return salaries = s
 }
