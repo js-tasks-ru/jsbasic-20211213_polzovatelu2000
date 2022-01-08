e15b50ef5dbef7e2cd15b4d2839f29c09c6455b5
Vlad <polzovatelu2000@gmail.com>
2022-01-08 17:33:56 +0300

    Add resolve

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,10 +5,13 @@
 //   }
 //   return users = mas
 // }
-let namify = (users) =>{
-  let mas = []
-    for(i in users){
-      mas.push(users[i].name)
-    }
-    return users = mas
-}
+
+// let namify = (users) =>{
+//   let mas = []
+//     for(i in users){
+//       mas.push(users[i].name)
+//     }
+//     return users = mas
+// }
+
+let namify = (users)=>users.map(item => item.name)
