c8f50f4e3bfaf05deb5d429272e7dcf64e9bf86e
Vlad <polzovatelu2000@gmail.com>
2022-01-08 17:30:00 +0300

    Transform name

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,14 @@
-function namify(users) {
-  // ваш код...
+// function namify(users) {
+//   let mas = []
+//   for(i in users){
+//     mas.push(users[i].name)
+//   }
+//   return users = mas
+// }
+let namify = (users) =>{
+  let mas = []
+    for(i in users){
+      mas.push(users[i].name)
+    }
+    return users = mas
 }
