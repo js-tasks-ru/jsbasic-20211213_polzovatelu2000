59a288c4203d60c1cb447ef9928c776d72c0f508
Vlad <polzovatelu2000@gmail.com>
2022-01-14 19:05:45 +0300

    Change resolve task 2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,6 +1,3 @@
 function filterRange(arr, a, b) {
-  let c = []
-  c.push(Math.abs(arr.indexOf(a)))
-  c.push(Math.abs(arr.indexOf(b)))
-  return c
+  return arr.filter(item => item>=a && item<=b);
 }
