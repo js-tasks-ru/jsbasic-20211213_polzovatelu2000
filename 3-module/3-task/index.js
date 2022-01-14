02c6a15fd16c937d23bf8d6d0a94db1b85a6a906
Vlad <polzovatelu2000@gmail.com>
2022-01-14 19:06:05 +0300

    filterRange

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
-  // ваш код...
+  if(str.indexOf("-")>=0){
+    while(true){
+      let c = str.slice(str.indexOf("-") + 1)
+      str = str.slice(0, str.indexOf("-"))
+      c = c[0].toUpperCase() + c.slice(1)
+      str = str + c;
+    if(str.indexOf("-") === -1)break;
+    }
+  }
+  return str
 }
