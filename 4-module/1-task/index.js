0f11575e0749f5c8637c5e2b6aca6cd80f8bf021
Vlad <polzovatelu2000@gmail.com>
2022-01-18 22:01:55 +0300

    makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul')
+  for(i in friends){
+    let li = document.createElement('li')
+    li.append(friends[i].firstName +" "+friends[i].lastName)
+    ul.append(li)
+  }
+  return ul
 }
